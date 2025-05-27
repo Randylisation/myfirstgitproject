7f28f990293f42ebab990decae0e2860ebb6d7bf
Randylisation <mala.jallow@outlook.com>
2025-05-27 16:13:28 +0200

    Uppdaterat med en ny rad

--- src/testingtesting/Hello.java
+++ src/testingtesting/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hejsan svejsan");
 
+        System.out.println("Detta är i helloDog");
+
 
         System.out.println("Testar efter uppladdning");
     }
